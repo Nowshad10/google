42b3d1d893817682fed6d5f17970507f651c5e8e
Nowshad Ahmed <nowshad.ahmed@hotmail.co.ukgit config user.name Nowshad10>
2022-03-16 12:34:56 +0000

    Added event listener

--- script2.js
+++ script2.js
@@ -5,5 +5,7 @@ function submit() {
     document.getElementById('content').textContent = ""
     let val = document.getElementById('search-field').value
     let newElement = document.createElement('script');
-    newElement.src = `https://www.googleapis.com/customserach/v1?key=AIzaSyCpYIvipKZbCoBwuRRL6QjKDOixJC1KAw0&cx=`
+    newElement.src = `https://www.googleapis.com/customserach/v1?key=AIzaSyCpYIvipKZbCoBwuRRL6QjKDOixJC1KAw0&cx=3e7f3bd4638a6ce39&q=${val}&callback=hndlr`
+    newElement.id = "mainscript"
+    document.body.appendChild(newElement);
 }
